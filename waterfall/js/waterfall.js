a8d35fcae30cfefb3e225daa6344c9eb3d30a620
LawrenceChen <chentongyu13@gmail.com>
2015-07-14 12:28:09 +0800

    fixed a bug

--- waterfall/js/waterfall.js
+++ waterfall/js/waterfall.js
@@ -42,6 +42,8 @@ $.getJSON("json/imageName0To29.json", function(json){
 });
 $('img').css('width', $('.photo_box').css('width'));
 $('img').attr('alt', '图片加载失败');
+$('.lefttitlebg').css('width', $('.photo_box').css('width'));
+$('.righttitlebg').attr('alt', '图片加载失败');
 //获取整个文档长宽
 var documentW = $(document).width();
 var documentH = $(document).height();
