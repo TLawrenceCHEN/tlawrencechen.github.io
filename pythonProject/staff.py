9c5afc53256e4311245eabd8d528af5c7a51bcd4
LawrenceChen <chentongyu13@gmail.com>
2015-09-25 10:30:29 +0800

    remove python test page

--- pythonProject/staff.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'TongyuChen'
-
-
-class Staff:
-    def __init__(self, name, age, position = None, salary = 0):
-        self.name = name
-        self.age = age
-        self.position = position
-        self.salary = salary
